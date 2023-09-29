e993aad5c6be443c5ba8e70485d4da93d9d59952
kzoeps <kzoepa@gmail.com>
2023-09-29 19:32:07 -0400

    search with query params

--- src/routes/phone-search/+page.svelte
+++ src/routes/phone-search/+page.svelte
@@ -1,8 +1,32 @@
 <script>
-	import { Card, Text, Button, Input, Flex } from '@svelteuidev/core';
+	import { SimpleGrid, Card, Text, Button, Input, Flex } from '@svelteuidev/core';
+	export let data;
 	let number = '';
-	const print_number = () => {
-		console.log(number);
+	const getName = (record) => {
+		return `${record['First Name']} ${record['Last Name']}`;
+	};
+
+	const getAddy = (record) => {
+		return `${record['Street Address']}, ${record['City']}, ${record['State']}, ${record['Zip']}`;
+	};
+
+	const handleNumber = (e) => {
+		let num = e.target.value
+		const split_number = num.split('').filter((n) => !isNaN(n) && n !== ' ');
+		console.log(split_number);
+		let res = ''
+		split_number.forEach((num,i) => {
+			if (i === 0) {
+				res = `(${num}`
+			} else if (i === 2) {
+				res += `${num}) `
+			} else if (i === 5) {
+				res += `${num}-`
+			} else if (i <= 9) {
+				res += num
+			}
+		})
+		number = res
 	};
 </script>
 
@@ -10,29 +34,32 @@
 	<form method="POST" action="/phone-search">
 		<Flex margin="auto" gap="sm">
 			<Input
-				type="number"
 				style="flex-grow: 2"
 				name="number"
+				on:input={handleNumber}
 				bind:value={number}
 				placeholder="Enter phone number"
 			/>
 			<Button type="submit">Search</Button>
 		</Flex>
 	</form>
-    <Card>
-		<Card.Section>
-			<Text><span style="font-weight: 400;">Name:</span> Karma Yoezer</Text>
-		</Card.Section>
-		<Card.Section>
-			<Text><span style="font-weight: 400;">Phone Number:</span> (213) 189 2930</Text>
-		</Card.Section>
-		<Card.Section>
-			<Text
-				><span style="font-weight: 400;">Address</span> 270 Mohegan Ave, 06320 CT New London</Text
-			>
-		</Card.Section>
-		<Card.Section>
-			<Text><span style="font-weight: 400;">Type</span>Rental Customer</Text>
-		</Card.Section>
-	</Card>
+	<SimpleGrid style="margin: 1em" cols={2} >
+		{#each data.records as record}
+			<Card style="padding: 2em">
+				<Card.Section style="margin-bottom: 1em">
+					<Text><span style="font-weight: 400;">Name:</span>{getName(record.fields)}</Text>
+				</Card.Section>
+				<Card.Section style="margin-bottom: 1em">
+					<Text
+						><span style="font-weight: 400;">Phone Number:</span>{record?.fields?.[
+							'Phone Number'
+						]}</Text
+					>
+				</Card.Section>
+				<Card.Section style="margin-bottom: 1em">
+					<Text><span style="font-weight: 400;">Address: </span>{getAddy(record.fields)}</Text>
+				</Card.Section>
+			</Card>
+		{/each}
+	</SimpleGrid>
 </main>
