8f99b87a1614acee6f6683c6a44978d63c12575e
kzoeps <kzoepa@gmail.com>
2023-10-09 12:34:04 -0400

    feat: add icons package
    stuff
    
    create icon button

--- src/routes/phone-search/+page.svelte
+++ src/routes/phone-search/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { SimpleGrid, Card, Text, Button, Input, Flex } from '@svelteuidev/core';
+	import MdiPen from 'virtual:icons/mdi/pen';
+
 	export let data;
 	let number = '';
 	const getName = (record) => {
@@ -14,21 +16,21 @@
 	// since it is saved like this in airtable and we need to filter and search for it in the exact fashion
 	// lemme know if you have a smarter way to do this.
 	const handleNumber = (e) => {
-		let num = e.target.value
+		let num = e.target.value;
 		const split_number = num.split('').filter((n) => !isNaN(n) && n !== ' ');
-		let res = ''
-		split_number.forEach((num,i) => {
+		let res = '';
+		split_number.forEach((num, i) => {
 			if (i === 0) {
-				res = `(${num}`
+				res = `(${num}`;
 			} else if (i === 2) {
-				res += `${num}) `
+				res += `${num}) `;
 			} else if (i === 5) {
-				res += `${num}-`
+				res += `${num}-`;
 			} else {
-				res += num
+				res += num;
 			}
-		})
-		number = res
+		});
+		number = res;
 	};
 </script>
 
@@ -46,23 +48,29 @@
 			<Button type="submit">Search</Button>
 		</Flex>
 	</form>
-	<SimpleGrid style="margin: 1em" cols={2} >
+	<SimpleGrid style="margin: 1em" cols={2}>
 		{#each data.records as record}
-			<Card style="padding: 2em">
-				<Card.Section style="margin-bottom: 1em">
-					<Text><span style="font-weight: bold;">Name: </span>{getName(record.fields)}</Text>
-				</Card.Section>
-				<Card.Section style="margin-bottom: 1em">
-					<Text
-						><span style="font-weight: bold;" >Phone Number: </span>{record?.fields?.[
-							'Phone Number'
-						]}</Text
-					>
-				</Card.Section>
-				<Card.Section style="margin-bottom: 1em">
-					<Text><span style="font-weight: bold;">Address: </span>{getAddy(record.fields)}</Text>
-				</Card.Section>
-			</Card>
+			<div style="position: relative">
+				<Card style="padding: 2em">
+					<Card.Section style="margin-bottom: 1em">
+						<Text><span style="font-weight: bold;">Name: </span>{getName(record.fields)}</Text>
+					</Card.Section>
+					<Card.Section style="margin-bottom: 1em">
+						<Text
+							><span style="font-weight: bold;">Phone Number: </span>{record?.fields?.[
+								'Phone Number'
+							]}</Text
+						>
+					</Card.Section>
+					<Card.Section style="margin-bottom: 1em">
+						<Text><span style="font-weight: bold;">Address: </span>{getAddy(record.fields)}</Text>
+					</Card.Section>
+				</Card>
+				<Button
+					style="width: 50px; border-radius: 50%; height: 50px; position: absolute; right: -10px; top: -10px"
+					><MdiPen /></Button
+				>
+			</div>
 		{/each}
 	</SimpleGrid>
 </main>
