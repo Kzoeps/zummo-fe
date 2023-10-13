3d02cd235f83f5060df834fcd44bc1bc0b823152
kzoeps <kzoepa@gmail.com>
2023-10-13 15:04:44 -0400

    feat: update recrod
    refactor: formatting of number

--- src/routes/phone-search/+page.svelte
+++ src/routes/phone-search/+page.svelte
@@ -10,7 +10,7 @@
 	const handleModalOpen = () => {
 		showModal = true;
 		modalData = data?.records?.[0]?.fields ? { ...data.records[0].fields } : {};
-	}
+	};
 	const getName = (record) => {
 		return `${record['First Name']} ${record['Last Name']}`;
 	};
@@ -21,32 +21,43 @@
 
 	const closeModal = () => {
 		showModal = false;
-	}
+	};
 
 	// all the logic below is to format the phone number like this: (214) 180-1323
 	// since it is saved like this in airtable and we need to filter and search for it in the exact fashion
 	// lemme know if you have a smarter way to do this.
-	const handleNumber = (e) => {
-		let num = e.target.value;
-		const split_number = num.split('').filter((n) => !isNaN(n) && n !== ' ');
-		let res = '';
-		split_number.forEach((num, i) => {
+	const formatNumber = (number) => {
+		const individualNumbers = number.split('').filter((n) => !isNaN(n) && n !== ' ');
+		let formattedNumber = '';
+		individualNumbers.forEach((num, i) => {
 			if (i === 0) {
-				res = `(${num}`;
+				formattedNumber = `(${num}`;
 			} else if (i === 2) {
-				res += `${num}) `;
+				formattedNumber += `${num})`;
+			} else if (i == 3) {
+				formattedNumber += ` ${num}`;
 			} else if (i === 5) {
-				res += `${num}-`;
+				formattedNumber += `${num}-`;
 			} else {
-				res += num;
+				formattedNumber += num;
 			}
 		});
-		number = res;
+		return formattedNumber;
+	};
+
+	const handleNumber = (e) => {
+		let num = e.target.value;
+		number = formatNumber(num);
+	};
+
+	const checkNumberValidity = (number) => {
+		const formattedNumber = formatNumber(number);
+		modalData['Phone Number'] = formattedNumber;
 	};
 </script>
 
 <main>
-	<form method="POST" action="/phone-search">
+	<form method="POST" action="?/searchRecord">
 		<Flex margin="auto" gap="sm">
 			<Input
 				style="flex-grow: 2"
@@ -57,6 +68,7 @@
 				placeholder="Enter phone number"
 			/>
 			<Button type="submit">Search</Button>
+			<Button>Create</Button>
 		</Flex>
 	</form>
 	<SimpleGrid style="margin: 1em" cols={2}>
@@ -78,7 +90,7 @@
 					</Card.Section>
 				</Card>
 				<Button
-				on:click={handleModalOpen}
+					on:click={handleModalOpen}
 					style="width: 50px; border-radius: 50%; height: 50px; position: absolute; right: -10px; top: -10px"
 					><MdiPen /></Button
 				>
@@ -86,16 +98,36 @@
 		{/each}
 	</SimpleGrid>
 	<Modal opened={showModal} on:close={closeModal} title="Details">
-        <TextInput label="First Name" name="First Name" bind:value={modalData['First Name']} />
-        <TextInput label="Last Name" name="Last Name" bind:value={modalData['Last Name']} />
-        <TextInput label="Phone Number" name="Phone Number" bind:value={modalData['Phone Number']} />
-		<TextInput label="Street Address" name="Street Address" bind:value={modalData['Street Address']} />
-		<TextInput label="City" name="City" bind:value={modalData['City']} />
-		<TextInput label="Zip" name="Zip" bind:value={modalData['Zip']} />
-		<TextInput label="State" name="State" bind:value={modalData['State']} />
-		<Flex gap="sm" mt={10} justify="right" >
-			<Button variant="outline" on:click={closeModal}>Cancel</Button>
-			<Button type="submit" on:click={() => console.log(data)}>Save</Button>
-		</Flex>
+		<form method="POST" action="?/updateRecord">
+			<input name="id" type="hidden" value={data.records[0].id} />
+			<TextInput
+				required
+				label="First Name"
+				name="First Name"
+				bind:value={modalData['First Name']}
+			/>
+			<TextInput required label="Last Name" name="Last Name" bind:value={modalData['Last Name']} />
+			<TextInput
+				required
+				label="Phone Number"
+				name="Phone Number"
+				maxlength="14"
+				bind:value={modalData['Phone Number']}
+				on:input={(e) => checkNumberValidity(e.target.value)}
+			/>
+			<TextInput
+				required
+				label="Street Address"
+				name="Street Address"
+				bind:value={modalData['Street Address']}
+			/>
+			<TextInput required label="City" name="City" bind:value={modalData['City']} />
+			<TextInput required label="Zip" name="Zip" bind:value={modalData['Zip']} />
+			<TextInput required label="State" name="State" bind:value={modalData['State']} />
+			<Flex gap="sm" mt={10} justify="right">
+				<Button variant="outline" on:click={closeModal}>Cancel</Button>
+				<Button type="submit" on:click={() => console.log(data)}>Save</Button>
+			</Flex>
+		</form>
 	</Modal>
 </main>
