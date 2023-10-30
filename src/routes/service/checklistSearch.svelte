12f5091da8a91851d56096f346550e5c3616561b
Alex Towle <alextowle@DESKTOP-ORUV582.conncoll.edu>
2023-10-30 12:37:50 -0400

    Adding Homepage and servicespages, also started integration for backend and frontend

--- src/routes/service/checklistSearch.svelte
+++ src/routes/service/checklistSearch.svelte
@@ -3,18 +3,28 @@
     import { createEventDispatcher } from "svelte";
 
     const dispatch = createEventDispatcher();
+    const headers = {
+        "Content-Type": "application/json"
+    }
+
+    let serviceRecordID = ''; //stores user input
 
     async function openTuneUpModal() {
         dispatch('open-tune-up-modal');
     }
 
     async function getRecordNumber() {
-        fetch("./get-services-by-id")
-        
+        // Make a fetch request with user input
+        fetch("http://127.0.0.1:5000/get-services-by-id", {method: "POST", body: JSON.stringify({"Service Bike ID": serviceRecordID}), headers })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response data as needed
+                console.log(data);
+            });
     }
 </script>
 
 <strong>Search for an existing service record using the record number</strong><br><br>
 
-<TextInput label="Service Record"/> <br>
-<Button on:click={getRecordNumber()} ripple>Search</Button> <br>
+<TextInput label="Service Record" bind:value={serviceRecordID}/> <br>
+<Button on:click={getRecordNumber} ripple>Search</Button> <br>
